2b9fbc29624d4936489b87e9c1df7ea0dfbec19f
Srv <srvsaikumar@gmail.com>
2025-06-27 22:46:59 +0530

    Added REST API at '/'

--- src/main/java/com/jenkins/firstapp/my_first_app/MyFirstAppApplication.java
+++ src/main/java/com/jenkins/firstapp/my_first_app/MyFirstAppApplication.java
@@ -2,9 +2,19 @@ package com.jenkins.firstapp.my_first_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyFirstAppApplication {
+
+	@GetMapping("/")
+	public ResponseEntity<String> helloWorld() {
+		return ResponseEntity.status(200).body("Hello");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstAppApplication.class, args);
